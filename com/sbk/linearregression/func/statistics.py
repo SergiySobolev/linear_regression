d3fba410a5d31a17b62dc107d6cc1ca8e581fc6c
Serhii Sobolev <sergiy.sobolev@gmail.com>
2018-11-25 01:16:45 +0200

    linear_correlation_coefficient

--- com/sbk/linearregression/func/statistics.py
+++ com/sbk/linearregression/func/statistics.py
@@ -11,9 +11,21 @@ def coefficient_of_determination(y, p_y):
 
 
 def pearson_correlation_coefficient(y, p_y):
+    assert (len(y) == len(p_y)), "Vectors must be same length"
     y_m = np.mean(y)
     p_y_m = np.mean(p_y)
     v1 = np.sum((y - y_m)*(p_y - p_y_m))
     v2 = np.sum((y - y_m)**2)
     v3 = np.sum((p_y - p_y_m)**2)
     return v1 / np.sqrt(v2 * v3)
+
+
+def linear_correlation_coefficient(y, p_y):
+    assert (len(y) == len(p_y)), "Vectors must be same length"
+    n = len(y)
+    y_s = np.sum(y)
+    p_y_s = np.sum(p_y)
+    v1 = n*np.sum(np.dot(y, p_y)) - y_s * p_y_s
+    v2 = np.sqrt(n * np.dot(y,y) - y_s**2)
+    v3 = np.sqrt(n * np.dot(p_y,p_y) - p_y_s ** 2)
+    return v1/(v2*v3)
--- tests/com/sbk/linearregression/func/test_statistics.py
+++ tests/com/sbk/linearregression/func/test_statistics.py
@@ -2,7 +2,8 @@ import unittest
 import numpy as np
 from parameterized import parameterized
 from sklearn.metrics import r2_score
-from com.sbk.linearregression.func.statistics import coefficient_of_determination, pearson_correlation_coefficient
+from com.sbk.linearregression.func.statistics import coefficient_of_determination, pearson_correlation_coefficient, \
+    linear_correlation_coefficient
 
 
 class TestStatistics(unittest.TestCase):
@@ -26,7 +27,7 @@ class TestStatistics(unittest.TestCase):
         actual = coefficient_of_determination(y, p_y)
         self.assertEqual(actual, expected)
 
-    def test_pearson_correlation_coefficient(self):
+    def test_pearson_correlation_coefficient_generated(self):
         len = 10
         max = 20
         y = np.random.randint(max, size=len)
@@ -34,3 +35,21 @@ class TestStatistics(unittest.TestCase):
         expected = np.corrcoef(y, p_y)[1,0]
         actual = pearson_correlation_coefficient(y, p_y)
         self.assertAlmostEqual(actual, expected)
+
+    @parameterized.expand([
+        ["first", (4, 15, 3, 10, 15, 9), (9, 17, 8, 11, 14, 12), 0.9395],
+        ["second", (1, 2, 3), (1, 2, 3), 1],
+        ["third", (1, 2, 3), (3, 2, 1), -1]
+    ])
+    def test_pearson_correlation_coefficient_generated(self, name, y, p_y, expected):
+        actual = pearson_correlation_coefficient(y, p_y)
+        self.assertAlmostEqual(actual, expected, places=2)
+
+    @parameterized.expand([
+        ["first", (4, 15, 3, 10, 15, 9), (9, 17, 8, 11, 14, 12), .93951],
+        ["second", (1, 2, 3), (1, 2, 3), 1],
+        ["third", (1, 2, 3), (3, 2, 1), -1]
+    ])
+    def test_linear_correlation_coefficient_parametrized(self, name, y, p_y, expected):
+        actual = linear_correlation_coefficient(y, p_y)
+        self.assertAlmostEqual(actual, expected, places=2)
